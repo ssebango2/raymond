851c73b3a52473fea463dd2c4d9e3467d67a9646
Charles Ryu <charles.ryu@gmail.com>
2024-07-24 13:31:56 -0700

    added linter and linted

--- src/app/ui/navbar.tsx
+++ src/app/ui/navbar.tsx
@@ -4,8 +4,8 @@ import Link from 'next/link';
 export default function Navbar() {
   return (
     <nav className="bg-blue-900 p-4 shadow-lg">
-      <div className="container mx-auto flex justify-between items-center">
-        <div className="text-white text-2xl font-bold">
+      <div className="container mx-auto flex items-center justify-between">
+        <div className="text-2xl font-bold text-white">
           <Link href="/">Vote for Raymond</Link>
         </div>
         <NavLinks />
