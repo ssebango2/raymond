114867f57cf35f55456a51161d78745c0231c8d6
Charles Ryu <charles.ryu@gmail.com>
2024-09-19 09:41:29 -0700

    removed comments

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,9 +1,7 @@
 'use client';
 
-// import { useState } from 'react';
 import styled from 'styled-components';
 import Image from 'next/image';
-// import Link from 'next/link';
 
 const PageContainer = styled.div`
   display: flex;
